bc8449008ba4c13b395d76bf18cccaa43cff13a6
sean-lgt <852951042@qq.com>
2022-10-29 13:52:32 +0800

    feat: 增加路由导航守卫，跳转404页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,68 +2,99 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/components/Home.vue'
 
 const routes = [{
-  path: '/',
-  redirect: '/welcome',
-  name: 'home',
-  meta: {
-    title: '首页',
+    path: '/',
+    redirect: '/welcome',
+    name: 'home',
+    meta: {
+      title: '首页',
+    },
+    component: Home,
+    // /system/user
+    children: [{
+        path: '/welcome',
+        name: 'welcome',
+        meta: {
+          title: '欢迎页',
+        },
+        component: () => import( /* webpackChunkName: "homeWelcome" */ '@/views/Home/welcome.vue')
+      }, {
+        path: '/system/user',
+        name: 'systemUser',
+        meta: {
+          title: '用户管理',
+        },
+        component: () => import( /* webpackChunkName: "homeWelcome" */ '@/views/System/user.vue')
+      },
+      {
+        path: '/system/menu',
+        name: 'systemMenu',
+        meta: {
+          title: '菜单管理',
+        },
+        component: () => import( /* webpackChunkName: "homeWelcome" */ '@/views/System/menu.vue')
+      },
+      {
+        path: '/system/role',
+        name: 'systemRole',
+        meta: {
+          title: '角色管理',
+        },
+        component: () => import( /* webpackChunkName: "homeWelcome" */ '@/views/System/role.vue')
+      },
+      {
+        path: '/system/dept',
+        name: 'systemDept',
+        meta: {
+          title: '部门管理',
+        },
+        component: () => import( /* webpackChunkName: "homeWelcome" */ '@/views/System/dept.vue')
+      },
+    ],
+  }, {
+    path: '/login',
+    name: 'login',
+    meta: {
+      title: '登录',
+    },
+    component: () => import( /* webpackChunkName: "homeWelcome" */ '@/views/Login/index.vue')
   },
-  component: Home,
-  // /system/user
-  children: [{
-      path: '/welcome',
-      name: 'welcome',
-      meta: {
-        title: '欢迎页',
-      },
-      component: () => import( /* webpackChunkName: "homeWelcome" */ '@/views/Home/welcome.vue')
-    }, {
-      path: '/system/user',
-      name: 'systemUser',
-      meta: {
-        title: '用户管理',
-      },
-      component: () => import( /* webpackChunkName: "homeWelcome" */ '@/views/System/user.vue')
-    },
-    {
-      path: '/system/menu',
-      name: 'systemMenu',
-      meta: {
-        title: '菜单管理',
-      },
-      component: () => import( /* webpackChunkName: "homeWelcome" */ '@/views/System/menu.vue')
-    },
-    {
-      path: '/system/role',
-      name: 'systemRole',
-      meta: {
-        title: '角色管理',
-      },
-      component: () => import( /* webpackChunkName: "homeWelcome" */ '@/views/System/role.vue')
-    },
-    {
-      path: '/system/dept',
-      name: 'systemDept',
-      meta: {
-        title: '部门管理',
-      },
-      component: () => import( /* webpackChunkName: "homeWelcome" */ '@/views/System/dept.vue')
-    },
-  ],
-}, {
-  path: '/login',
-  name: 'login',
-  meta: {
-    title: '登录',
+  {
+    name: "404",
+    path: "/:pathMatch(.*)",
+    meta: { title: "未找到该页面" },
+    component: () => import("@/views/NotFound/404.vue"),
   },
-  component: () => import( /* webpackChunkName: "homeWelcome" */ '@/views/Login/index.vue')
-}]
-
+]
 
 const router = createRouter({
   // history: createWebHistory(process.env.BASE_URL),
   history: createWebHashHistory(),
   routes
 })
+
+
+// 判断当前地址是否可以访问
+const checkPermission = (path) => {
+  const hasPermission = router.getRoutes().filter(route => route.path == path).length;
+  if (hasPermission) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// 导航守卫
+router.beforeEach((to, from, next) => {
+  if (checkPermission(to.path)) {
+    console.log('🚀【寄哪里发大水发】', );
+    next()
+  } else {
+    next()
+    // next('/404')
+  }
+})
+
+
+
 
 export default router
---
+++ src/views/NotFound/404.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="page-404">
+    <img src="@/assets/images/404.jpeg" alt="" />
+    <el-button class="btn-404" size="medium" @click="$router.push('/')"
+      >返回首页</el-button
+    >
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.page-404 {
+  width: 100vw;
+  height: 100vh;
+  img {
+    width: 100%;
+    height: 100%;
+    display: block;
+  }
+  .btn-404 {
+    position: fixed;
+    bottom: 100px;
+  }
+}
+</style>
